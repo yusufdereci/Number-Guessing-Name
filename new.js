df6f9ab160321dd6ee32ac19d68e2b43d8bb91a1
yusufdereci <dereciyusuf0@gmail.com>
2026-02-08 12:58:30 +0300

    Add files via upload

---
+++ new.js
@@ -0,0 +1,49 @@
+const min = 1;
+const max = 100;
+
+// Rastgele sayı üret
+let answer = Math.floor(Math.random() * (max - min + 1)) + min;
+
+let tries = 0;
+
+// HTML elemanları
+const input = document.getElementById("guessInput");
+const button = document.getElementById("guessBtn");
+const message = document.getElementById("message");
+const triesText = document.getElementById("tries");
+
+// Butona tıklama olayı
+button.addEventListener("click", checkGuess);
+
+function checkGuess() {
+
+    let guess = Number(input.value);
+
+    if (!guess) {
+        message.textContent = "Lütfen bir sayı gir!";
+        message.style.color = "red";
+        return;
+    }
+
+    tries++;
+
+    if (guess < answer) {
+        message.textContent = "📉 Çok düşük!";
+        message.style.color = "orange";
+    }
+    else if (guess > answer) {
+        message.textContent = "📈 Çok yüksek!";
+        message.style.color = "orange";
+    }
+    else {
+        message.textContent = "🎉 Doğru bildin!";
+        message.style.color = "green";
+
+        input.disabled = true;
+        button.disabled = true;
+    }
+
+    triesText.textContent = "Deneme sayısı: " + tries;
+
+    input.value = "";
+}
